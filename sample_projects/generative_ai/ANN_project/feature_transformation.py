8d4c3ad71cb527065fc7b9f879e841e39a3d009e
Adrita Sharma <adritasharma.developer@gmail.com>
2025-08-09 19:27:41 +0530

    refactor

--- sample_projects/generative_ai/ANN_project/feature_transformation.py
+++ sample_projects/generative_ai/ANN_project/feature_transformation.py
@@ -4,59 +4,58 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pickle
 
-data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "Churn_Modelling.csv")
-data = pd.read_csv(data_path)
+class FeatureTransformer:
+    def __init__(self):
+        self.data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "Churn_Modelling.csv")
+        self.output_data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "processed_data.pkl")
+        self.scaler_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "scaler.pkl")
+        self.data = None
+        self.label_encoder_gender = LabelEncoder()
+        self.oneHotEncoder_geography = OneHotEncoder()
+        self.scaler = StandardScaler()
+        self.X_train_scaled = None
+        self.X_test_scaled = None
+        self.y_train = None
+        self.y_test = None
+        self.run()
 
+    def load_data(self):
+        self.data = pd.read_csv(self.data_path)
 
-## Feature Transformation using Sklearn
+    def preprocess(self):
+        self.data = self.data.drop(['RowNumber', 'CustomerId', 'Surname'], axis=1)
 
-## Preprocessing the data
-data = data.drop(['RowNumber', 'CustomerId', 'Surname'], axis=1)
+    def encode_gender(self):
+        self.data['Gender'] = self.label_encoder_gender.fit_transform(self.data['Gender'])
 
-## Here Geography and gender are categorical variables
+    def encode_geography(self):
+        geography_encoded = self.oneHotEncoder_geography.fit_transform(self.data[['Geography']])
+        geography_encoded_df = pd.DataFrame(
+            geography_encoded.toarray(),
+            columns=self.oneHotEncoder_geography.get_feature_names_out(['Geography'])
+        )
+        self.data = pd.concat([self.data.drop('Geography', axis=1), geography_encoded_df], axis=1)
 
-## Encode categorical variables i.e convert male and female to 0 and 1
-label_encoder_gender = LabelEncoder()
+    def save_processed_data(self):
+        with open(self.output_data_path, 'wb') as f:
+            pickle.dump(self.data, f)
 
-data['Gender'] = label_encoder_gender.fit_transform(data['Gender'])
+    def split_and_scale(self):
+        X = self.data.drop('Exited', axis=1)
+        y = self.data['Exited']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.X_train_scaled = self.scaler.fit_transform(X_train)
+        self.X_test_scaled = self.scaler.transform(X_test)
+        self.y_train = y_train
+        self.y_test = y_test
+        with open(self.scaler_path, 'wb') as f:
+            pickle.dump(self.scaler, f)
 
-print(data.head())
+    def run(self):
+        self.load_data()
+        self.preprocess()
+        self.encode_gender()
+        self.encode_geography()
+        self.save_processed_data()
+        self.split_and_scale()
 
-## Encode Geography using OneHot Encoding (OHE)
-oneHotEncoder_geography = OneHotEncoder()
-geography_encoded = oneHotEncoder_geography.fit_transform(data[['Geography']])
-
-print(geography_encoded.toarray())
-
-geography_encoded_df = pd.DataFrame(geography_encoded.toarray(), columns=oneHotEncoder_geography.get_feature_names_out(['Geography']))
-
-print(geography_encoded_df)
-
-## Combine OHE columns with the original dataframe
-data = pd.concat([data.drop('Geography', axis=1), geography_encoded_df], axis=1)
-
-print(data)
-
-## save file using pickle
-output_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "processed_data.pkl")
-with open(output_file, 'wb') as f:
-    pickle.dump(data, f)                        
-
-
-## Divide the dataset into independent and dependent variables
-
-X = data.drop('Exited', axis=1)
-y = data['Exited']  
-
-## Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-## Scale the features
-scaler = StandardScaler()   
-X_train_scaled = scaler.fit_transform(X_train)
-X_test_scaled = scaler.transform(X_test)    
-
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data", "scaler.pkl"), 'wb') as f:
-    pickle.dump(scaler, f)  
-
-    
